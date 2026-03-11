1f7fdeaf212f56d76404a65fae9ed20eefd00ef7
David Ogunbanjo <85774740+DeReals@users.noreply.github.com>
2026-03-10 23:12:32 -0400

    done Q. 1a

--- asn2.lisp
+++ asn2.lisp
@@ -1,1 +1,24 @@
-(+ 12 1)
+#|
+Authors: David Ogunbanjo & Denis Moroz
+Description: This file contains the answers to the ASN2 assignment questions.
+Date: 3/10/2026
+|#
+
+;; Question 1
+
+;; 1a: Read three numbers and return a list composed of those three numbers.
+
+(let ((num1 (read)) (num2 (read)) (num3 (read)))
+    (list num1 num2 num3)
+)
+
+;; 1b: 
+
+
+;; 1c: 
+
+;; Question 2
+
+;; Question 3
+
+;; Question 4

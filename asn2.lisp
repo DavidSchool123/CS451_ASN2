ba4fbcfa30a6e69585d3a4591f51abd2b80b5075
David Ogunbanjo <85774740+DeReals@users.noreply.github.com>
2026-03-11 01:20:45 -0400

    completed Q 3

--- asn2.lisp
+++ asn2.lisp
@@ -5,11 +5,11 @@ Date: 3/10/2026
 |#
 
 ;; Question 1
-
+#|
 ;; 1a: Read three numbers and return a list composed of those three numbers.
 
-(let ((num1 (read)) (num2 (read)) (num3 (read)))
-    (list num1 num2 num3)
+(let ((num1 (read)) (num2 (read)) (num3 (read))) ;; reads input
+    (list num1 num2 num3)                        ;; return as list
 )
 
 
@@ -18,13 +18,29 @@ Date: 3/10/2026
 element of the cons is the sum of both numbers, and the second element of the cons is the product of 
 both numbers |#
 
-(let ((num1 (read)) (num2 (read)))
-    (let ((sum (+ num1 num2)) (product (* num1 num2)))
-        (cons sum product)
+(let ((num1 (read)) (num2 (read)))                     ;; reads input
+    (let ((sum (+ num1 num2)) (product (* num1 num2))) ;; perform operations
+        (cons sum product)                             ;; returns cons in (sum . product) format
     )   
 )
-
+|#
 ;; 1c: 
+#|Read one positive integer and return a list composed of all positive odd numbers less or equal to 
+it (if the number is odd).   For example, if you read the value 8 or 7 the return should be (1 3 5 7) |#
+
+(let ((*num* (read)) (oddList '()) (*dec* 1))  ;; read the number, make the empty list, and the decrement value
+    (if (plusp *num*)   ;; check if it is positive
+        (loop while (> *num* 0) do   ;; loop break condition
+            (if (not (zerop (rem *num* 2)))    ;; if the number is odd, add to the list
+                (push *num* oddList)
+            ) 
+            (setq *num* (- *num* (abs *dec*)))  ;; decrement num by 1
+        )
+    ) ;; update the value of num
+    (print oddList)
+    oddList
+)
+
 
 ;; Question 2
 

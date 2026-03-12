6520732ede57e26d1ac9965f85e4664f6c72599a
Denis7755 <vmoroz@loyola.edu>
2026-03-12 17:21:17 -0400

    Update asn2.lisp

--- asn2.lisp
+++ asn2.lisp
@@ -28,18 +28,17 @@ both numbers |#
 #|Read one positive integer and return a list composed of all positive odd numbers less or equal to 
 it (if the number is odd).   For example, if you read the value 8 or 7 the return should be (1 3 5 7) |#
 #|
-(let ((*num* (read)) (oddList '()) (*dec* 1))  ;; read the number, make the empty list, and the decrement value
-    (if (plusp *num*)   ;; check if it is positive
-        (loop while (> *num* 0) do   ;; loop break condition
-            (if (not (zerop (rem *num* 2)))    ;; if the number is odd, add to the list
-                (push *num* oddList)
-            ) 
-            (setq *num* (- *num* (abs *dec*)))  ;; decrement num by 1
-        )
-    ) ;; update the value of num
-    (print oddList)
-    oddList
-)|#
+
+(let ((num (read)) (odd-list '()) (dec 1))  ;; read the number, make the empty list, and the decrement value
+    (if (plusp num)  ;; checks if it is positive
+        (loop whole (> num 0) do ;; the loop break condition
+            (if (not (zerop (rem num 2))) ;;if number is odd, add to list
+                (push num odd-list))
+            (setq num (-num dec)))) ;; lower the value of num by 1 as dec is 1
+    odd-list)
+
+
+|#
 
 
 ;; Question 2
@@ -53,7 +52,7 @@ a greeting and return true.
 (LLM return true in a list)
 If both are different, then do not print anything and return false. |#
 #|
-(defun reader (preText)
+(defun 2a (preText)
     (let ((userText (read)))
         (if (string-equal preText userText)
             (progn
@@ -72,7 +71,7 @@ two numbers.   If the first parameter is an asterisk, then you must return the m
 numbers.    If the first parameter is a forward slash, then you must return the division of the numbers.
 the first parameter is anything else, then print an error message and return zero.   |#
 #|
-(defun calculator (sign num1 num2)
+(defun 2b (sign num1 num2)
     (declare (integer num1 num2))
     (declare (string sign))
 
@@ -91,7 +90,7 @@ the first parameter is anything else, then print an error message and return zer
 #|Create a function that receives as a parameter a cons in the format (number1 . number2) and 
 returns the sum of both numbers inside that cons.    For example (2c (cons 2 3)) must return 5.  |#
 #|
-(defun conFunc (consN)
+(defun 2c (consN)
     (+ (car consN) (cdr consN))
 )
 
@@ -101,15 +100,21 @@ returns the sum of both numbers inside that cons.    For example (2c (cons 2 3))
 #|Create a non-recursive function that returns the factorial of a number passed as a parameter. |#
 
 
-
-
+(defun 2d (num)
+    (declare (integer num)) ;;don't think this is needed
+    (let ((result 1) ;; set the result to 1 & i (<- this is the counter) to num 
+          (i num))
+         (do ()                        ;; each loop does result *= i
+             ((<= i 1) result) ;; this ends the loop when i <= i; and returns the result
+             (setq result (* result i)) ;; for each factor of num; it updates the result by muipltying it by i. (eX: start: 1 & i = 5 -> then results is: 5)
+             (setq i (- i 1))))) ;; for reach num factoried i gets reduced by 1 until it gets to i =1 leading to then end of the loop. 
 ;; 2e
 ;; Question: Create a function that receives two lists of numbers as parameters. If the lists do not have the same size, the function ends and returns an empty list.(LLM return “wrong size”)Otherwise, you must return another list that is composed of the sum of the elements on both list in order. For example, (2e '(1 2 3) '(4 5 6)) should return (5 7 9).
-(defun listsum (x1 x2)
+(defun 2e (x1 x2)
     (if (/= (length x1) (length x2)) ;; returns empty list if the 2 lists are not the same in length
         nil
         (cons (+ (car x1) (car x2)) ;; adding the lists
-              (listsum (cdr x1) (cdr x2))))) ;; returns the sum of the two lists (in a new list)
+              (2e (cdr x1) (cdr x2))))) ;; returns the sum of the two lists (in a new list)
         
 
 
